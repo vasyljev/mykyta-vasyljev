e47e58bd0146ad351a0510cd5a43f5b7fcd60f88
Mykyta Vasyljev <mykyta.vasyljev@gmail.com>
2018-10-04 19:38:35 +0300

    v 1.6

--- tanatos/script.js
+++ tanatos/script.js
@@ -1,17 +1,17 @@
-jQuery('.scroll_to').click(function(e){
+$('.scroll_to').click(function(e){
         var jump = $(this).attr('href');
         var new_position = $(jump).offset();
        $('html, body').stop().animate({ scrollTop: new_position.top }, 800);
        e.preventDefault();
     });
 
-    $(document).ready(function(){
+$(document).ready(function(){
 
 	$(window).scroll(function() {
 		if($(this).scrollTop() != 0) {
-			$('#arrow-to-top-link').fadeIn();
+			$('#arrow-to-top').fadeIn();
 		} else {
-			$('#arrow-to-top-link').fadeOut();
+			$('#arrow-to-top').fadeOut();
 		}
 	});
 
@@ -19,10 +19,4 @@ jQuery('.scroll_to').click(function(e){
 		$('body, html').animate({scrollTop:0},800);
 	});
 
-	$(".comment-notes").text('Коментар для зворотнього зв\'язку');
-	//$("#reply-title").text('Ваш отклик');
-	$("label[for='author']").text("Ваше ім\'я");
-	$("label[for='email']").text("Електронна пошта");
-	$("label[for='comment']").text("Повідомлення");
- 	$("#submit").attr('value', 'Відправити коментар');
 });
