d5f943793df9960d17ee2b2b2305227ee573fa06
Mykyta Vasyljev <mykyta.vasyljev@gmail.com>
2018-10-10 12:02:53 +0300

    v 2.2

--- tanatos/script.js
+++ tanatos/script.js
@@ -6,32 +6,46 @@ $('.scroll_to').click(function(e){
     });
 
 $(document).ready(function(){
+	var firstAnimHeight;
+	var secondAnimHeight;
+	var thirdAnimHeight;
 
+if ($(this).width() > 940) {
+	firstAnimHeight = 100;
+	secondAnimHeight = 1100;
+	thirdAnimHeight = 2800;
+} if (($(this).width() < 940)&&($(this).width() > 720)) {
+	firstAnimHeight = 150;
+	secondAnimHeight = 1000;
+	thirdAnimHeight = 2700;
+} else if ($(this).width() < 720)  {
+	firstAnimHeight = 300;
+	secondAnimHeight = 1500;
+	thirdAnimHeight = 3800;
+}
 	$(window).scroll(function() {
 		if($(this).scrollTop() != 0) {
 			$('#arrow-to-top-link').fadeIn();
 		} else {
 			$('#arrow-to-top-link').fadeOut();
 		}
+		if ($(this).scrollTop() > firstAnimHeight) {
+			$('.service').fadeIn().animate({right: 0, opacity: 1},600);
+		}
+		if ($(this).scrollTop() > secondAnimHeight) {
+			$('.portfolio').fadeIn().animate({opacity: 1},800);
+			
+		}
+		if ($(this).scrollTop() > thirdAnimHeight) {
+			$('.left-anim-ti').fadeIn().animate({left: 0},600);
+			$('.right-anim-ti').fadeIn().animate({right: 0},600);
+			
+		}
 	});
 
 	$('#arrow-to-top-link').click(function() {
 		$('body, html').animate({scrollTop:0},800);
 	});
-	$(window).scroll(function() {
-		if ($(this).scrollTop() > 100) {
-			$('.service').fadeIn().animate({right: 0, opacity: 1},600);
-		}
-		if ($(this).scrollTop() > 1000) {
-			$('.portfolio').fadeIn().animate({opacity: 1},800);
-			
-		}
-		if ($(this).scrollTop() > 2800) {
-			$('.left-anim-ti').fadeIn().animate({left: 0},800);
-			$('.right-anim-ti').fadeIn().animate({right: 0},800);
-			
-		}
-	}); 	
 	
 });
 
