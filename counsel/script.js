33d47f0958320381c1435027e2c27c2d6384f4e8
Mykyta Vasyljev <mykyta.vasyljev@gmail.com>
2018-10-14 14:20:18 +0300

    v 2.8

--- counsel/script.js
+++ counsel/script.js
@@ -1,48 +1,45 @@
 $(document).ready(function(){
-  setTimeout(function() {
-    $('#slider').addClass('animate');
-  }, 1500);  
-  var blockStatus = true;
-  $(window).scroll(function() {    
-        if ($(this).scrollTop() > 290) {
+    setTimeout(function() {
+        $('#slider').addClass('animate');
+    }, 1500);  
+    var firstAnimHeight;
+    var secondAnimHeight;
+    var thirdAnimHeight;
+    var blockStatus = true;
+    $(window).scroll(function() {
+        if($(this).width() > 870) {
+            firstAnimHeight = 300;
+            secondAnimHeight = 1700;
+            thirdAnimHeight = 2300;
+        } else if ($(this).width() < 870) {
+            firstAnimHeight = 400;
+            secondAnimHeight = 1900;
+            thirdAnimHeight = 3100;
+        }  
+        if ($(this).scrollTop() > firstAnimHeight) {
             $('#graph').addClass('anim-graph');
         }
-        if ($(this).scrollTop() > 1700) {
+        if ($(this).scrollTop() > secondAnimHeight) {
             $('.service-img').addClass('anim-service-img');
         }
-       
-        if ($(this).scrollTop() > 2300) {           
+       function numbersAnimate(blockName, endValue) {          
+            console.log(blockName);
+            $({numberValue: 0}).animate({numberValue:  endValue}, { 
+                duration: 1000,
+                easing: "linear", 
+                step: function(val) {  
+                    $(blockName).html(Math.ceil(val));
+                } 
+            })
+       }
+       if ($(this).scrollTop() > thirdAnimHeight) {           
             $(function () {                 
                 if(blockStatus){  
-                    blockStatus = false;                 
-                    $({numberValue: 0}).animate({numberValue: 7890}, { 
-                        duration: 1000,
-                        easing: "linear", 
-                        step: function(val) {  
-                            $("#first-stat").html(Math.ceil(val));
-                        } 
-                    });
-                    $({numberValue: 0}).animate({numberValue: 9999}, { 
-                        duration: 1000,
-                        easing: "linear", 
-                        step: function(val) {  
-                            $("#second-stat").html(Math.ceil(val));
-                        } 
-                    });
-                    $({numberValue: 0}).animate({numberValue: 1509}, { 
-                        duration: 1000,
-                        easing: "linear", 
-                        step: function(val) {  
-                            $("#third-stat").html(Math.ceil(val));
-                        } 
-                    });
-                    $({numberValue: 0}).animate({numberValue: 5972}, { 
-                        duration: 1000,
-                        easing: "linear", 
-                        step: function(val) {  
-                            $("#fourth-stat").html(Math.ceil(val));
-                        } 
-                    });
+                    blockStatus = false; 
+                    numbersAnimate('#first-stat', 7890);
+                    numbersAnimate('#second-stat', 9999); 
+                    numbersAnimate('#third-stat', 1509);
+                    numbersAnimate('#fourth-stat', 5972); 
                 }      
             }); 
 
