00aee99a74eb203b794f9836c7970b521621db2f
Mykyta Vasyljev <mykyta.vasyljev@gmail.com>
2018-10-17 22:07:23 +0300

    v 2.9

--- tanatos/script.js
+++ tanatos/script.js
@@ -3,7 +3,16 @@ $('.scroll_to').click(function(e){
         var new_position = $(jump).offset();
        $('html, body').stop().animate({ scrollTop: new_position.top }, 800);
        e.preventDefault();
-    });
+	});
+$('#arrow-to-top-link').click(function() {
+	$('body, html').animate({scrollTop:0},600);
+	// $('#arrow-to-top-link').animate({opacity:0},600);
+});
+if($(window).scrollTop() > 100) {
+	$('#arrow-to-top-link').animate({opacity:1},600);
+} else {
+	$('#arrow-to-top-link').animate({opacity:0},600);
+}
 
 $(document).ready(function(){
 	var firstAnimHeight;
@@ -43,9 +52,7 @@ if ($(this).width() > 940) {
 		}
 	});
 
-	$('#arrow-to-top-link').click(function() {
-		$('body, html').animate({scrollTop:0},800);
-	});
+	
 	var index = -1
 	var $div = $('.team-block .team-av')
 
